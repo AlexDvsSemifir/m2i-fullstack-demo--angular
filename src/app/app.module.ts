53bbfbe8e5fbb43d861d4de04728f5a61ec82e4a
AlexDvsSemifir <alexandre.devos@semifir.com>
2022-06-27 14:15:14 +0200

    feat(demo): Ajout démos binding :sparkles:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+import { UserComponent } from './user/user.component';
+import { CarteVisiteComponent } from './carte-visite/carte-visite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentComponent,
+    UserComponent,
+    CarteVisiteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
