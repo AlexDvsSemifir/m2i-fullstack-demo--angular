46753870d836f68a6cfd55f6b9685315cdc5bf47
AlexDvsSemifir <alexandre.devos@semifir.com>
2022-06-28 17:02:11 +0200

    feat: Ajout demo reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
@@ -17,6 +17,7 @@ import { ButtonMenuComponent } from './button-menu/button-menu.component';
 import { HorlogeComponent } from './horloge/horloge.component';
 import { HorlogeMenuComponent } from './horloge-menu/horloge-menu.component';
 import { ListeCoursesComponent } from './liste-courses/liste-courses.component';
+import { ReactiveListeCoursesComponent } from './reactive-liste-courses/reactive-liste-courses.component';
 
 @NgModule({
   declarations: [
@@ -33,12 +34,14 @@ import { ListeCoursesComponent } from './liste-courses/liste-courses.component';
     ButtonMenuComponent,
     HorlogeComponent,
     HorlogeMenuComponent,
-    ListeCoursesComponent
+    ListeCoursesComponent,
+    ReactiveListeCoursesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
